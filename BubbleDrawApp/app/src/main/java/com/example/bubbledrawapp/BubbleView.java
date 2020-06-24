9c8601656c7a718ee4d146c365d320d64efcae21
Joakim <33222649+JoakimFKK@users.noreply.github.com>
2020-06-24 12:28:10 +0200

    teknisk set færdig

--- BubbleDrawApp/app/src/main/java/com/example/bubbledrawapp/BubbleView.java
+++ BubbleDrawApp/app/src/main/java/com/example/bubbledrawapp/BubbleView.java
@@ -5,49 +5,64 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.widget.ImageView;
 import android.view.View;
 import java.util.ArrayList;
-import java.util.List;
+import android.os.Handler;
 import java.util.Random;
-import java.util.logging.Handler;
-import java.util.logging.LogRecord;
 
 public class BubbleView extends ImageView implements View.OnTouchListener {
     private Random rand = new Random();
     private ArrayList<Bubble> bubbleList;
     private int size = 50;
     private int delay = 33;
-    private int color;
     private Paint myPaint = new Paint();
-    private Handler h = new Handler() {
-        @Override
-        public void publish(LogRecord logRecord) {
-
-        }
-
-        @Override
-        public void flush() {
-
-        }
-
-        @Override
-        public void close() throws SecurityException {
-
-        }
-    }; // Enables us to work with threading
+    private Handler h = new Handler(); // Enables us to work with threading
 
 
     public BubbleView(Context context, AttributeSet attributeSet) {
         super(context, attributeSet);
         bubbleList = new ArrayList<Bubble>();
+        // testBubbles();
+        setOnTouchListener(this);
     }
+
+    private Runnable r = new Runnable() {
+        @Override
+        public void run() {
+            for (Bubble b : bubbleList)
+                b.update();
+            invalidate();
+        }
+    };
 
     protected void onDraw(Canvas canvas) {
         for (Bubble b : bubbleList) {
             b.draw(canvas);
         }
+        h.postDelayed(r, delay);
     }
+
+    public void testBubbles() {
+        for (int i = 0; i < 100; i++) {
+            bubbleList.add(new Bubble(
+                    rand.nextInt(600),
+                    rand.nextInt(400),
+                    rand.nextInt(50))
+            );
+        }
+        invalidate();  // Fungerer som repaint();
+    }
+
+    @Override
+    public boolean onTouch(View view, MotionEvent motionEvent) {
+        for (int i = 0; i < motionEvent.getPointerCount(); i++) {
+            bubbleList.add(new Bubble((int)motionEvent.getX(), (int)motionEvent.getY(), (rand.nextInt(size) + size)));
+        }
+        return true;
+    }
+
 
     private class Bubble {
         // Instance variable
@@ -55,7 +70,7 @@ public class BubbleView extends ImageView implements View.OnTouchListener {
         private int size;
         private int color;
         private int xspeed, yspeed;
-        private final int MAX_SPEED = 5;
+        private final int MAX_SPEED = 15;
 
         // Constructor
         public Bubble(int x, int y, int size) {
@@ -72,6 +87,7 @@ public class BubbleView extends ImageView implements View.OnTouchListener {
                 yspeed = rand.nextInt(MAX_SPEED * 2 + 1) - MAX_SPEED;  // Negative values moves the bubbles to the left
             } while (xspeed == 0 && yspeed == 0);
         }
+
 
         // Drawing the bubble to the canvas.
         public void draw(Canvas canvas) {
